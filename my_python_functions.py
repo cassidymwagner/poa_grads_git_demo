b681d65e5ee0ed5f7e39d2c764516e628a073830
pcarlip <peter.carlip@gmail.com>
2024-10-24 13:32:30 -0700

    default formatting

--- my_python_functions.py
+++ my_python_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def calc_mean(x):
     """Calculate the mean of an array of numbers.
     Parameters
@@ -10,9 +11,10 @@ def calc_mean(x):
     -------
     mean : float
         The mean of the input array of numbers.
-    
+
     """
     return np.mean(x)
+
 
 def calc_median(x: np.ndarray) -> float:
     """Calculate the median of an array of numbers.
@@ -24,12 +26,14 @@ def calc_median(x: np.ndarray) -> float:
     -------
     median : float
         The median of the input array of numbers.
-    
+
     """
     return np.median(x)
 
+
 def calc_std(x):
     return np.std(x)
+
 
 def calc_add(x, y):
     """Calculate the sum of two numbers.
@@ -46,16 +50,20 @@ def calc_add(x, y):
     """
     return x + y
 
+
 def calc_sub(x, y):
     return x - y
+
 
 def print_my_name(name):
     if not isinstance(name, str):
         raise ValueError("The input must be a string.")
     print("Hello, my name is", name)
 
+
 def print_something_nice():
     print("You are doing a great job!")
 
+
 def print_age(birth_year):
-    print(f'I am {2024 - birth_year} years old.')
+    print(f"I am {2024 - birth_year} years old.")
